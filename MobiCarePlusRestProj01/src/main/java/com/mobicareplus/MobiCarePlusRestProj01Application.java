6d4e1f7249f94882bf4884d9358f54f6ab9879a0
Ajay <bhartiajay808@gmail.com>
2025-03-17 08:27:44 +0530

    First commit for project structure

---
+++ MobiCarePlusRestProj01/src/main/java/com/mobicareplus/MobiCarePlusRestProj01Application.java
@@ -0,0 +1,13 @@
+package com.mobicareplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MobiCarePlusRestProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MobiCarePlusRestProj01Application.class, args);
+	}
+
+}
---
+++ MobiCarePlusRestProj01/src/test/java/com/mobicareplus/MobiCarePlusRestProj01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobicareplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobiCarePlusRestProj01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
